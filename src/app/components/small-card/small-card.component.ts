f7ef3ea4bddac491be677baf5a2b8ae073563b6d
fabiobrasileiroo <fabio.h591@gmail.com>
2024-02-06 16:54:31 -0400

    estudo

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
+  // Corrigindo a declaração da interface
 
-  constructor() { }
+  // Corrigindo o nome da variável e a tipagem
+  listas: any[];
 
-  ngOnInit(): void {
+  constructor() {
+    interface Card {
+      img: string;
+      title: string;
+      description: string;
+    }
+    this.listas = [
+      {
+        img: 'https://blog.wijman.net/content/images/2022/08/post-image-linux-sleep-hibernation.png',
+        title: 'Linux dorme em quanto ',
+        description: 'blabla',
+      },
+      {
+        img: 'https://blog.wijman.net/content/images/2022/08/post-image-linux-sleep-hibernation.png',
+        title: 'Título 2',
+        description: 'bla bla',
+      },
+      {
+        img: 'https://blog.wijman.net/content/images/2022/08/post-image-linux-sleep-hibernation.png',
+        title: 'Título 2',
+        description: 'bla bla',
+      },
+
+      // Adicione mais objetos conforme necessário
+    ];
   }
 
+  ngOnInit(): void {}
 }
