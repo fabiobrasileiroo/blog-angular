bdab354d922214bf8ed1fdec555201c4084f533c
Fabio Brasileiro <fabiotrocados@debian-BULLSEYE-live-builder-AMD64>
2024-02-06 17:59:51 -0400

    estudo

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
-
+  @Input()
+  photoCover:string ='https://media.licdn.com/dms/image/D4E12AQGx_XxlH8rHLw/article-cover_image-shrink_423_752/0/1699328092463?e=1712793600&v=beta&t=QQpri70Jc2-U5AKa7oPpBPfE0D9npppYI02e86AgW7Y'
+  @Input()
+  cardTitle: string = 'Novo angular'
+  @Input()
+  cardDescription: string = 'O angular 17 estar top'
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -9,33 +9,36 @@ export class SmallCardComponent implements OnInit {
   // Corrigindo a declaração da interface
 
   // Corrigindo o nome da variável e a tipagem
-  listas: any[];
-
+  // listas: any[];
+  @Input()
+  photoCover:string = 'https://infopunch.files.wordpress.com/2013/07/linux_pingwin_041.jpg?w=640'
+  @Input()
+  cardTitle:string = 'Linux achar windows um comedia'
   constructor() {
-    interface Card {
-      img: string;
-      title: string;
-      description: string;
-    }
-    this.listas = [
-      {
-        img: 'https://blog.wijman.net/content/images/2022/08/post-image-linux-sleep-hibernation.png',
-        title: 'Linux dorme em quanto ',
-        description: 'blabla',
-      },
-      {
-        img: 'https://blog.wijman.net/content/images/2022/08/post-image-linux-sleep-hibernation.png',
-        title: 'Título 2',
-        description: 'bla bla',
-      },
-      {
-        img: 'https://blog.wijman.net/content/images/2022/08/post-image-linux-sleep-hibernation.png',
-        title: 'Título 2',
-        description: 'bla bla',
-      },
+    // interface Card {
+    //   img: string;
+    //   title: string;
+    //   description: string;
+    // }
+    // this.listas = [
+    //   {
+    //     img: 'https://blog.wijman.net/content/images/2022/08/post-image-linux-sleep-hibernation.png',
+    //     title: 'Linux dorme em quanto ',
+    //     description: 'blabla',
+    //   },
+    //   {
+    //     img: 'https://blog.wijman.net/content/images/2022/08/post-image-linux-sleep-hibernation.png',
+    //     title: 'Título 2',
+    //     description: 'bla bla',
+    //   },
+    //   {
+    //     img: 'https://blog.wijman.net/content/images/2022/08/post-image-linux-sleep-hibernation.png',
+    //     title: 'Título 2',
+    //     description: 'bla bla',
+    //   },
 
-      // Adicione mais objetos conforme necessário
-    ];
+    //   // Adicione mais objetos conforme necessário
+    // ];
   }
 
   ngOnInit(): void {}
